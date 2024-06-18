6cd098f861e8c9588c6384a789d5b718407cf5df
guimoz11 <guimoz2005@gmail.com>
2024-06-18 16:04:16 -0300

    Create AppChallenge.java

---
+++ AppChallenge/AppChallenge/src/appchallenge/AppChallenge.java
@@ -0,0 +1,19 @@
+
+package appchallenge;
+
+
+import Interface.TelaLogin;
+
+/**
+ *
+ * @author guilherme.vcmoz
+ */
+public class AppChallenge {
+
+    public static void main(String[] args) {
+        TelaLogin p = new TelaLogin();
+        p.setVisible(true);
+       
+    }
+    
+}
